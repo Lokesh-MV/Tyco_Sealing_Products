bcbdfaddf3e59bcf5c5a4af56023dc4642dcb263
Lokesh -MV <108014905+Lokesh-MV@users.noreply.github.com>
2023-09-20 23:17:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+    
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+    
+            everyitem.addEventListener('mouseover', function(e){
+    
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+    
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+    
+    
+            })
+        });
+    
+    }
+    // end if innerWidth
+    }); 
+
+
+    let sections = document.querySelectorAll('section');
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offSet = sec.offsetTop - 300;
+            let height = sec.offsetHeight;
+            if (top >= offSet && top <= offSet + height){
+                sec.classList.add('show-animate');
+            }
+            // else{
+            //     sec.classList.remove('show-animate');
+            // }
+        })
+    }
